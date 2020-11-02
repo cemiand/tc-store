bc6b5e93a5bce5a7ca6e1647ba75503fe1fcc06d
Emiliano <emiliano_ac88@hotmail.com>
2020-11-02 19:08:37 -0300

    1st commit

---
+++ front/webpack.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+      filename: "bundle.js",
+      path: __dirname + "/../back/public",
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    context: __dirname,
+    module: {
+      rules: [
+        {
+          test: /jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: "babel-loader",
+          query: {
+            presets: ["@babel/preset-react", "@babel/env"],
+          },
+        },
+        {
+          test: /.s[ac]ss$/i,
+          use: [
+            // Creates style nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+            {
+              loader: "sass-loader",
+              options: {
+                implementation: require("sass"),
+              },
+            },
+          ],
+        },
+      ],
+    },
+    devtool: "source-map",
+  };
