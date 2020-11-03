0586a528733f0ac00cdaddf6f442d3b61de3612b
Nahuel <bonadernahuel@hotmail.com>
2020-11-03 20:19:00 -0300

    server reloaded

--- back/app/configure/parsing-middleware.js
+++ back/app/configure/parsing-middleware.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-module.exports = function (app) {
+module.exports = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 };
