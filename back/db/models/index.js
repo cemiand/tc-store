5bca288ef18fdecd9911b669f932a14959285374
DiGarber <diegogarber09@gmail.com>
2020-11-03 17:37:48 -0300

    models index

---
+++ back/db/models/index.js
@@ -0,0 +1,9 @@
+const Brand = require("./Brand");
+const Category = require('./Category');
+const Order = require('./Order');
+const Product = require('./Product');
+const Review = require('./Review');
+const User = require('./User');
+
+
+module.exports = { Brand, Category, User, Order, Review, Product };
