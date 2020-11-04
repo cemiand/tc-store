98707066ae3c51b72d9e18d0b5c25ce2141bd702
Aletartaglia <alejandro.tartaglia92@gmail.com>
2020-11-04 12:19:07 -0300

    actualizado y refactoreo de rutas usuarios

--- back/controllers/usuariosController.js
+++ back/controllers/usuariosController.js
@@ -49,9 +49,16 @@ const usuariosController = {
       .catch(err => {
         res.status(500).send(err)
       })
-
+  },
+  findAll(req, res) {
+    User.find({})
+      .then(users => {
+        res.send(users)
+      })
+      .catch(err => {
+        res.status(500).send(err);
+      });
   }
-
 }
 
 module.exports = usuariosController
