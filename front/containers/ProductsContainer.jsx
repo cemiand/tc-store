939c7cc48099a932951238e29ccec0aae82ea602
Emiliano <emiliano_ac88@hotmail.com>
2020-11-04 12:44:48 -0300

    react-redux update

--- front/containers/ProductsContainer.jsx
+++ front/containers/ProductsContainer.jsx
@@ -1,12 +1,12 @@
 import React, {useEffect} from 'react';
 import Products from "../components/Products"
 import {fetchProducts} from "../src/store/actions/productAction"
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 
 const ProductContainer = () => {
 
-    const dispatch = useDispatch();
+    const dispatch = useDispatch()
 
     const { products } = useSelector(state => state.products);
 
