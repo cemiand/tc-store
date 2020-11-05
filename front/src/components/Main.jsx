fa5916287d7d35e91240cf9467e3c6f268bd135b
Nahuel <bonadernahuel@hotmail.com>
2020-11-05 20:06:57 -0300

    persistencia ok

--- front/src/components/Main.jsx
+++ front/src/components/Main.jsx
@@ -1,31 +1,28 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom'
-import ProductsContainer from "../containers/ProductsContainer"
-import SingleProductContainer from '../containers/SingleProductContainer';
-import LoginContainer from "../containers/LoginContainer"
-import CarruselContainer from '../containers/CarruselContainer';
-import NavbarContainer from '../containers/NavbarContainer';
-import RoundedImages from './Categories';
-import Categories from './Categories';
-import '../assets/style.scss'
-
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import ProductsContainer from "../containers/ProductsContainer";
+import SingleProductContainer from "../containers/SingleProductContainer";
+import LoginContainer from "../containers/LoginContainer";
+import CarruselContainer from "../containers/CarruselContainer";
+import NavbarContainer from "../containers/NavbarContainer";
+import RoundedImages from "./Categories";
+import Categories from "./Categories";
 
 const Main = () => {
-    return (
-
-        <React.Fragment>
-            <NavbarContainer />
-            <br />
-            <CarruselContainer />
-            {/* <Categories /> */}
-            <Switch>
-                <Route exact path="/product/:id" component={SingleProductContainer} />
-                <Route exact path="/products" component={ProductsContainer} />
-                {/* <Route exact path="/login" component={LoginContainer} /> */}
-            </Switch>
-            {/* <Footer /> ACA IRIA EL FOOTER */}
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <NavbarContainer />
+      <br />
+      <CarruselContainer />
+      {/* <Categories /> */}
+      <Switch>
+        <Route exact path="/product/:id" component={SingleProductContainer} />
+        <Route exact path="/products" component={ProductsContainer} />
+        {/* <Route exact path="/login" component={LoginContainer} /> */}
+      </Switch>
+      {/* <Footer /> ACA IRIA EL FOOTER */}
+    </React.Fragment>
+  );
+};
 
 export default Main;
