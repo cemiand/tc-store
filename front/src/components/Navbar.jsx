5e3364b57b8ff95a31507e67d3eb779648c62e0b
Aletartaglia <alejandro.tartaglia92@gmail.com>
2020-11-05 17:02:57 -0300

    lo del dropwdown

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"
 import { Button, Modal, Nav, Navbar, Form, FormControl } from 'react-bootstrap'
 import LoginContainer from '../containers/LoginContainer'
 
@@ -12,32 +11,40 @@ export default () => {
   return (
 
     <Navbar bg="dark" variant="dark" expand="lg">
-
       <Navbar.Brand href="#home">Tc-Store</Navbar.Brand>
-
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-
         </Nav>
-        <div className="divSearchlupita">
-          <Form style={{ width: "100%", display: "flex", alignItems: "center" }}>
+        <div>
+          <Form inline style={{ width: "100%", }}>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Link to=""><span className="lupita"><img src="lupita2.png" className="lupitaimg" alt=""></img></span></Link>
+            <Button variant="outline-success">Search</Button>
           </Form>
         </div>
-
-        <Nav.Link href="#link" className="cart">
-          <Button variant="success" onClick={handleShow}>
-            Sign In
+        {userlog ? <>
+          <Dropdown>
+            <Dropdown.Toggle variant="" id="dropdown-basic">
+              <Link to="#" style={{ textDecoration: "none", color: "white", margin: " 0 5px" }}> {userlog}  <i className="fas fa-user-circle"> </i></Link>
+            </Dropdown.Toggle>
+            <Dropdown.Menu style={{ backgroundColor: "#343a40" }}>
+              <Dropdown.Item ><Link to="#" style={{ textDecoration: "none", color: "white", backgroundColor: "#343a40" }}><b> Favoritos</b></Link></Dropdown.Item>
+              <Dropdown.Item ><Link to="#" onClick={logout} style={{ textDecoration: "none", color: "white", backgroundColor: "#343a40" }}>Log Out</Link></Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </>
+          :
+          <>
+            <Button variant="success" onClick={handleShow}>
+              Sign In
           </Button>
-          <Modal show={show} onHide={handleClose}>
-            {<LoginContainer handleClose={handleClose} />}
+            <Modal show={show} onHide={handleClose}>
+              {<LoginContainer handleClose={handleClose} />}
 
-          </Modal>
-
-        </Nav.Link>
+            </Modal>
+          </>
+        }
         <Nav.Link href="#link" className="cart">Cart</Nav.Link>
       </Navbar.Collapse>
 
@@ -47,6 +54,27 @@ export default () => {
 
   );
 }
+
+
+/* {userlog ? <>
+            <Dropdown>
+              <Dropdown.Toggle variant="" id="dropdown-basic">
+                <Link to="/movies" style={{ textDecoration: "none", color: "white", margin: " 0 5px" }}> {userlog}  <i className="fas fa-user-circle"> </i></Link>
+              </Dropdown.Toggle>
+              <Dropdown.Menu style={{ backgroundColor: "#343a40" }}>
+                <Dropdown.Item ><Link to="/movies/favorites" style={{ textDecoration: "none", color: "white", backgroundColor: "#343a40" }}><b> Favoritos</b></Link></Dropdown.Item>
+                <Dropdown.Item ><Link to="/login" onClick={logout} style={{ textDecoration: "none", color: "white", backgroundColor: "#343a40" }}>Log Out</Link></Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </>
+            :
+            <>
+              <Link to="/movies/login" style={{ textDecoration: "none", color: "white", margin: " 0 5px" }}>Log In <i className="fas fa-user-circle"></i></Link>
+              <Link to="/movies/register" style={{ textDecoration: "none", color: "white", margin: " 0 5px" }}>Sign In</Link>
+            </>
+          } */
+
+
 
 /*   <Navbar bg="dark" variant="dark">
     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
