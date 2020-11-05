b486ec5260d1e3ecf513251b561e586fc9877855
Emiliano <emiliano_ac88@hotmail.com>
2020-11-05 16:38:30 -0300

    imagenes front y algo del carrusel

--- front/src/containers/LoginContainer.jsx
+++ front/src/containers/LoginContainer.jsx
@@ -7,5 +7,5 @@ export default ({ handleClose }) => {
   const handleSign = () => {
     document.querySelector(".cont").classList.toggle("s-signup");
   };
-  return <Login handleSign={handleSign} handleCloe={handleClose} />;
+  return <Login handleSign={handleSign} handleClose={handleClose} />;
 };
