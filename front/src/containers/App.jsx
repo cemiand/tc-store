7e89fd319cfcf7816a200df71c2e5721e89ea2da
Nahuel <bonadernahuel@hotmail.com>
2020-11-05 21:22:29 -0300

    Error de inicio resuelto

--- front/src/containers/App.jsx
+++ front/src/containers/App.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Main from "../components/Main";
 import { fetchUser } from "../store/actions/usersAction";
 import "../assets/style.scss";
 
 export default () => {
   const dispatch = useDispatch();
-  useEffect(() => dispatch(fetchUser()));
+
+  const { singleUser } = useSelector((state) => state.usersReducer);
+  useEffect(() => {
+    if (!singleUser) dispatch(fetchUser());
+  });
 
   return <Main />;
 };
--- front/src/containers/NavbarContainer.jsx
+++ front/src/containers/NavbarContainer.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import Navbar from "../components/Navbar"
-import { useDispatch, useSelector } from 'react-redux';
-import { logoutUser } from '../store/actions/usersAction'
-import useInput from "../hooks/useInput"
-
+import React from "react";
+import Navbar from "../components/Navbar";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutUser } from "../store/actions/usersAction";
+import useInput from "../hooks/useInput";
 
 const NavbarContainer = () => {
   const dispatch = useDispatch();
-  
-  const logout = () => {
-    dispatch(logoutUser())
-  }
-  
-  const { singleUser } = useSelector(state => state.usersReducer);
 
-  const {handleChange, inputs} = useInput();
-  console.log(inputs)
+  const logout = () => {
+    dispatch(logoutUser());
+  };
+
+  const { singleUser } = useSelector((state) => state.usersReducer);
+
+  const { handleChange, inputs } = useInput();
+
   return (
-    <Navbar userlog={singleUser} logout={logout} handleChange={handleChange} filterValue = {inputs.search}/>
-  )
-}
+    <Navbar
+      userlog={singleUser}
+      logout={logout}
+      handleChange={handleChange}
+      filterValue={inputs.search}
+    />
+  );
+};
 
 export default NavbarContainer;
