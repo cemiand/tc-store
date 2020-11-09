2a7cc1ced957fc9f30d79523cf25b68b7aed8038
Emiliano <emiliano_ac88@hotmail.com>
2020-11-09 13:28:55 -0300

    view del carrito

--- front/src/components/Main.jsx
+++ front/src/components/Main.jsx
@@ -4,21 +4,23 @@ import ProductsContainer from "../containers/ProductsContainer";
 import SingleProductContainer from "../containers/SingleProductContainer";
 import CarruselContainer from "../containers/CarruselContainer";
 import NavbarContainer from "../containers/NavbarContainer";
-import RoundedImages from "./Categories";
-import Categories from "./Categories";
+// import RoundedImages from "./Categories";
+// import Categories from "./Categories";
 import FilteredProductsContainer from "../containers/FilteredProductsContainer";
+import CartContainer from "../containers/CartContainer";
 
 const Main = () => {
   return (
     <React.Fragment>
       <NavbarContainer />
       <br />
-      <CarruselContainer />
+      {/* <CarruselContainer /> */}
       {/* <Categories /> */}
       <Switch>
         <Route path="/products/:id" component={SingleProductContainer} />
         <Route exact path="/products" component={ProductsContainer} />
         <Route path="/:search" component={FilteredProductsContainer}/>
+        <Route exact path="/cart" component={CartContainer} />
         <Redirect from="/" to="/products" />
       </Switch>
       {/* <Footer /> ACA IRIA EL FOOTER */}
